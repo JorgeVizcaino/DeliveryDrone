2c72c927456b300bc37b1192e4fa2c47340f3c7b
Jorge Vizcaino <jfvizcaino@hotmail.com>
2021-02-19 15:18:11 -0500

    operate drones simultaneously

---
+++ Delivery/Execptions/DroneExecption.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delivery.Extension
+{
+    [Serializable]
+    public class DroneExecption : Exception
+    {
+        public DroneExecption()
+        {
+
+        }
+        public DroneExecption(string name)
+            : base(String.Format("Invalid Student Name: {0}", name))
+        {
+
+        }
+    }
+}
